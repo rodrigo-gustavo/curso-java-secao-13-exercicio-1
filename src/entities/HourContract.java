eacb92f1272ed7700c17e929cce8c72e55e3fd29
Rodrigo Gustavo <2338.gustavo@gmail.com>
2021-06-08 14:24:53 -0300

    Exercicio 01 seção 13 criado

---
+++ src/entities/HourContract.java
@@ -0,0 +1,47 @@
+package entities;
+
+import java.util.Date;
+
+public class HourContract {
+	
+	private Integer hours;
+	private Date date;
+	private Double valuePerHour;
+	
+	public HourContract() {
+	}
+	
+	public HourContract(Integer hours, Date date, Double valuePerHour) {
+		this.hours = hours;
+		this.date = date;
+		this.valuePerHour = valuePerHour;
+	}
+
+	public Integer getHours() {
+		return hours;
+	}
+
+	public void setHours(Integer hours) {
+		this.hours = hours;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Double getValuePerHour() {
+		return valuePerHour;
+	}
+
+	public void setValuePerHour(Double valuePerHour) {
+		this.valuePerHour = valuePerHour;
+	}
+	
+	public double totalValue() {
+		return valuePerHour * hours;
+	}
+}
